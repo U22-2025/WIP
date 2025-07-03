c8f9c4422140933014cbb974aace79d4ccb7e288
v! <149035689+s27x4@users.noreply.github.com>
2025-07-03 10:45:52 +0900

    fix: rename pops_flag to pop_flag

--- wip_dissector.lua
+++ wip_dissector.lua
@@ -21,7 +21,7 @@ local f_type = ProtoField.uint8("wip.type", "Type", base.DEC, {
 -- フラグフィールド
 local f_weather_flag = ProtoField.bool("wip.weather_flag", "Weather Flag", 8, nil, 0x08)
 local f_temperature_flag = ProtoField.bool("wip.temperature_flag", "Temperature Flag", 8, nil, 0x10)
-local f_pops_flag = ProtoField.bool("wip.pops_flag", "PoPs Flag", 8, nil, 0x20)
+local f_pop_flag = ProtoField.bool("wip.pop_flag", "PoP Flag", 8, nil, 0x20)
 local f_alert_flag = ProtoField.bool("wip.alert_flag", "Alert Flag", 8, nil, 0x40)
 local f_disaster_flag = ProtoField.bool("wip.disaster_flag", "Disaster Flag", 8, nil, 0x80)
 local f_ex_flag = ProtoField.bool("wip.ex_flag", "Extension Flag", 8, nil, 0x01)
@@ -53,7 +53,7 @@ local f_ext_data = ProtoField.string("wip.ext_data", "Extension Data")
 -- プロトコルフィールドの登録
 wip_proto.fields = {
     f_version, f_packet_id, f_type,
-    f_weather_flag, f_temperature_flag, f_pops_flag, f_alert_flag, f_disaster_flag,
+    f_weather_flag, f_temperature_flag, f_pop_flag, f_alert_flag, f_disaster_flag,
     f_ex_flag, f_day, f_reserved,
     f_timestamp, f_area_code, f_checksum,
     f_weather_code, f_temperature, f_pops,
@@ -147,7 +147,7 @@ function wip_proto.dissector(buffer, pinfo, tree)
     local type_val = extract_bits_le(buffer(0, 4), 16, 3)
     local weather_flag = extract_bits_le(buffer(0, 4), 19, 1)
     local temperature_flag = extract_bits_le(buffer(0, 4), 20, 1)
-    local pops_flag = extract_bits_le(buffer(0, 4), 21, 1)
+    local pop_flag = extract_bits_le(buffer(0, 4), 21, 1)
     local alert_flag = extract_bits_le(buffer(0, 4), 22, 1)
     local disaster_flag = extract_bits_le(buffer(0, 4), 23, 1)
     local ex_flag = extract_bits_le(buffer(0, 4), 24, 1)
@@ -162,7 +162,7 @@ function wip_proto.dissector(buffer, pinfo, tree)
     local flag_tree = control_tree:add(buffer(2, 2), "Flags")
     flag_tree:add_le(f_weather_flag, buffer(2, 1)):append_text(" (" .. weather_flag .. ")")
     flag_tree:add_le(f_temperature_flag, buffer(2, 1)):append_text(" (" .. temperature_flag .. ")")
-    flag_tree:add_le(f_pops_flag, buffer(2, 1)):append_text(" (" .. pops_flag .. ")")
+    flag_tree:add_le(f_pop_flag, buffer(2, 1)):append_text(" (" .. pop_flag .. ")")
     flag_tree:add_le(f_alert_flag, buffer(2, 1)):append_text(" (" .. alert_flag .. ")")
     flag_tree:add_le(f_disaster_flag, buffer(2, 1)):append_text(" (" .. disaster_flag .. ")")
     flag_tree:add_le(f_ex_flag, buffer(3, 1)):append_text(" (" .. ex_flag .. ")")
