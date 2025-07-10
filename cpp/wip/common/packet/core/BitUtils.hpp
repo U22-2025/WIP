3f667ea0fbd8f5626845d50c7a79e7c24db93350
v! <149035689+s27x4@users.noreply.github.com>
2025-07-10 16:18:52 +0900

    MSVC向けにbits_to_bytes関数を追加

--- cpp/wip/common/packet/core/BitUtils.hpp
+++ cpp/wip/common/packet/core/BitUtils.hpp
@@ -1,5 +1,8 @@
 #pragma once
 #include <cstdint>
+#ifdef _MSC_VER
+#  include <intrin.h>
+#endif
 #include "Exceptions.hpp"
 
 namespace packet {
@@ -14,6 +17,18 @@ inline uint64_t extract_bits(uint64_t bitstr, int start, int length) {
 
 inline uint64_t extract_rest_bits(uint64_t bitstr, int start) {
     return bitstr >> start;
+}
+
+inline int bits_to_bytes(uint64_t bits) {
+#ifdef _MSC_VER
+    unsigned long idx;
+    if (_BitScanReverse64(&idx, bits)) {
+        return static_cast<int>((idx + 8) / 8);
+    }
+    return 1;
+#else
+    return bits ? static_cast<int>((64 - __builtin_clzll(bits) + 7) / 8) : 1;
+#endif
 }
 
 } // namespace packet
