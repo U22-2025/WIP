32ffc8e74605babd111e1b48b8024e43babcbc61
v! <szk.27x@gmail.com>
2025-07-14 23:57:37 +0900

    debugをオフに

--- start_servers.bat
+++ start_servers.bat
@@ -5,14 +5,23 @@ echo WIPサーバー起動中...
 set CURRENT_DIR=%cd%
 set PYTHONPATH=%CURRENT_DIR%;%PYTHONPATH%
 
+@REM wt ^
+@REM   new-tab cmd /k "cd /d %CURRENT_DIR% && conda activate U22-WIP && python python/launch_server.py --weather --debug" ^
+@REM   ; split-pane -V cmd /k "cd /d %CURRENT_DIR% && conda activate U22-WIP && python python/launch_server.py --query --debug" ^
+@REM   ; focus-pane -t 0 ^
+@REM   ; split-pane -H cmd /k "cd /d %CURRENT_DIR% && conda activate U22-WIP && python python/launch_server.py --location --debug" ^
+@REM   ; focus-pane -t 1 ^
+@REM   ; split-pane -H cmd /k "cd /d %CURRENT_DIR% && conda activate U22-WIP && python python/launch_server.py --report --debug" ^
+@REM   ; split-pane -V cmd /k "cd /d %CURRENT_DIR% && conda activate U22-WIP && python python/application/map/start_fastapi_server.py --debug" ^
+
 wt ^
-  new-tab cmd /k "cd /d %CURRENT_DIR% && conda activate U22-WIP && python python/launch_server.py --weather --debug" ^
-  ; split-pane -V cmd /k "cd /d %CURRENT_DIR% && conda activate U22-WIP && python python/launch_server.py --query --debug" ^
+  new-tab cmd /k "cd /d %CURRENT_DIR% && conda activate U22-WIP && python python/launch_server.py --weather" ^
+  ; split-pane -V cmd /k "cd /d %CURRENT_DIR% && conda activate U22-WIP && python python/launch_server.py --query" ^
   ; focus-pane -t 0 ^
-  ; split-pane -H cmd /k "cd /d %CURRENT_DIR% && conda activate U22-WIP && python python/launch_server.py --location --debug" ^
+  ; split-pane -H cmd /k "cd /d %CURRENT_DIR% && conda activate U22-WIP && python python/launch_server.py --location" ^
   ; focus-pane -t 1 ^
-  ; split-pane -H cmd /k "cd /d %CURRENT_DIR% && conda activate U22-WIP && python python/launch_server.py --report --debug" ^
-  ; split-pane -V cmd /k "cd /d %CURRENT_DIR% && conda activate U22-WIP && python python/application/map/start_fastapi_server.py --debug" ^
+  ; split-pane -H cmd /k "cd /d %CURRENT_DIR% && conda activate U22-WIP && python python/launch_server.py --report" ^
+  ; split-pane -V cmd /k "cd /d %CURRENT_DIR% && conda activate U22-WIP && python python/application/map/start_fastapi_server.py" ^
 start http://localhost:5000
 
 echo すべてのサーバーが起動しました。
